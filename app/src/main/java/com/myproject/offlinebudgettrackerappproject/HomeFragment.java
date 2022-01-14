df0a61ef53b9f595c190e5a8b991a70d1783972d
yakusugi <yosukeito1987@gmail.com>
2022-01-14 21:32:56 +0900

    commit jan 24 2022

--- app/src/main/java/com/myproject/offlinebudgettrackerappproject/HomeFragment.java
+++ app/src/main/java/com/myproject/offlinebudgettrackerappproject/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.myproject.offlinebudgettrackerappproject;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -72,7 +74,7 @@ public class HomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)                                                                {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.home_recycler_view);
         recyclerView.setHasFixedSize(true);
